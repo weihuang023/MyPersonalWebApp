6136d2f955b3fc2b412375af214059f667d99985
Wei Huang <Whuang@1800flowers.com>
2017-02-26 17:18:56 -0800

    Add project files.

---
+++ MyPersonalWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyPersonalWebApp.Startup))]
+namespace MyPersonalWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
